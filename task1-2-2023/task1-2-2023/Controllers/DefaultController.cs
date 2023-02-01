43cffc8c07844b218cac09667a18ccebb80ed33a
05-ayahalzyout <115452706+05-ayahalzyout@users.noreply.github.com>
2023-02-01 16:44:13 +0200

    task

---
+++ task1-2-2023/task1-2-2023/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task1_2_2023.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            List<Student> emp = new List<Student>();
+            emp.Add(new Student { ID = 5, Name = "Ayah", Major = "Network", Faculity = "balqa'a" });
+            emp.Add(new Student { ID = 2, Name = "Rama", Major = "Finace", Faculity = "Yurmouk" });
+
+            return View(emp);
+        }
+    }
+}
---
+++ task1-2-2023/task1-2-2023/Student .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace task1_2_2023
+{
+    public class Student
+    {
+        
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Major { get; set; }
+        public string Faculity { get; set; }
+
+    }
+}
